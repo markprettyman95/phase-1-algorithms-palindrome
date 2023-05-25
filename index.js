7116590d9804338c912466f32b87e00098984234
Mark Prettyman <markiesmyname@Marks-iMac.local>
2023-05-24 17:22:59 -0700

    algorithm practice

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+// The function isPalindrome() will return true if
+// the argument passed is a word that can be spelled
+// the same way reveresed i.e. "racecar"
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  if(word === word.split("").reverse().join("")) {
+    return true
+  } else {
+    return false
+  };
+};
+
+
+
+
+
+
 
 /* 
-  Add your pseudocode here
+if the word (which is a string) equals sthe string that is operated on, 
+return true
+else
+return false
 */
 
 /*
-  Add written explanation of your solution here
+The function isPalindrome() determines whether or not a word
+is a Palindrome by passing the argument given (which should be a string)
+through an if loop, given the condition that the string matches its reciprocal
+I used Google to find the methods I needed to
+1. split the string so that each letter becomes an element
+2. use .reverse method to join the string
+3. .join concatenates the results to reform the word, but in reverse
 */
 
 // You can run `node index.js` to view these console logs
